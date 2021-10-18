b77d6831ac73208c86af3b7de9078c66fbaa77e8
yurayarosh <yura12323@gmail.com>
2021-10-18 10:25:00 +0300

    pre render html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render, hydrate } from 'react-dom'
 import App from './App'
 import './assets/styles/app.scss'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+// render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
+
+const rootElement = document.getElementById('root')
+
+if (rootElement.hasChildNodes()) {
+  hydrate(<App />, rootElement)
+} else {
+  render(<App />, rootElement)
+}
